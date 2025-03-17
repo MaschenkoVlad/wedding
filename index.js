8bbb43bd746437516e1ae76b54aff6d9b709d159
Vladislav <1995talant@gmail.com>
2025-03-17 11:27:44 +0200

    fix css

--- index.js
+++ index.js
@@ -6,20 +6,18 @@ document.getElementById('form').addEventListener('submit', function(e) {
     
     const radios = document.getElementsByName('visit');
 
-    let attend = 'no';
+    let attend = 'No';
     for (var radio of radios) {
         if (radio.checked) {
             attend = radio.value;
         }
-    }  
+    }
 
-    const formUrl = 'https://docs.google.com/forms/u/0/d/1bL0IgrQaSQEbTmsKTKXLC3ENm-WoeVqlQiGGdLlR20c/previewResponse';
+    const formUrl = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfgmIz4BqJCdNAsCHOMFIzzF9BL76647FSe_gRb8Y2mw0NO-Q/formResponse';
     const data = new FormData();
     data.append('entry.1533925188', attend);
     data.append('entry.58696487', guestFullName);
     data.append('entry.229057109', guestCoupleFullName);
-
-    console.log("????", data);
     
     fetch(formUrl, {
       method: 'POST',
